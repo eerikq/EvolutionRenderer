18ae46543a0d836678bd7a4bf167f4d33eb8fdaf
eerikq <120007693+eerikq@users.noreply.github.com>
2026-08-04 12:15:15 +0200

    added sdl, installed vulkan at school pc

--- xmake.lua
+++ xmake.lua
@@ -2,9 +2,20 @@
 add_rules("mode.debug", "mode.release")
 add_rules("plugin.compile_commands.autoupdate")
 
+-- toolchain
 set_languages("c++20")
 set_toolchains("clang")
 
+-- libraries
+add_requires("libsdl3")
+--add_requires("imgui v1.92.5-docking", {
+--    configs = {
+--        sdl3 = true,
+--        sdl3_renderer = true,
+--    },
+--})
+
 target("EvolutionRenderer")
-set_kind("binary")
-add_files("src/*.cpp")
+    set_kind("binary")
+    add_files("src/*.cpp")
+    add_packages("libsdl3")
