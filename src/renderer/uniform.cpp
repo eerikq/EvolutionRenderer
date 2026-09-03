8e2293e444e4ae93e3ead47e9bf84e0b5cc69d73
eerikq <120007693+eerikq@users.noreply.github.com>
2026-09-03 10:28:45 +0200

    how funny is it that in the "let there be spin", i actually pushed it without said spin. haha, lol.

--- src/renderer/uniform.cpp
+++ src/renderer/uniform.cpp
@@ -31,7 +31,7 @@ namespace uniform {
         float time = std::chrono::duration<float, std::chrono::seconds::period>(currentTime - startTime).count();
 
         UniformBufferObject ubo {
-            .model = glm::rotate(glm::mat4(1.0f), glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f)),
+            .model = glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f)),
             .view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f)),
             .proj = glm::perspective(glm::radians(45.0f), swapchain_extent.width / (float)swapchain_extent.height, 0.1f, 10.0f),
         };
